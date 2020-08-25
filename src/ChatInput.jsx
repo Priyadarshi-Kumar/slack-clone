f707f88b87bf0378f7a4fe8c3bd999cfa8e775f5
Priyadarshi Kumar <Priyadarshi.Kumar@yulu.bike>
2020-08-25 14:24:15 +0530

    final build

--- src/ChatInput.jsx
+++ src/ChatInput.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./ChatInput.css";
+import { useStateValue } from "./StateProvider";
+import db from "./firebase";
+import firebase from "firebase";
 
-function ChatInput() {
+function ChatInput({ channelName, channelId }) {
+    const [input, setInput] = useState("");
+    const [{ user }] = useStateValue();
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if (channelId) {
+            db.collection("room").doc(channelId).collection("messages").add({
+                message: input,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                user: user.displayName,
+                userImage: user.photoURL
+            });
+        }
+        setInput("");
+    };
     return (
         <div className="chatInput">
             <form>
-                <input type="text"/>
+                <input
+                    type="text"
+                    placeholder={`Messafe #${channelName}`}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button type="submit" onClick={sendMessage}>SEND</button>
             </form>
         </div>
-    )
+    );
 }
 
-export default ChatInput
+export default ChatInput;
